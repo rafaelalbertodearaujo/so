f3d25104aa3253e26cce779d3af74a2fdd2d735c
rafaelalbertodearaujo <rafaelalbertola02@gmail.com>
2026-08-18 20:54:02 -0300

    export do status

--- status.c
+++ status.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
+/*
+   Funcionando normalmente:    00000000
+   Erro de inicializacao:      00000001
+   Dispositivo nao encontrado: 00000010
+   Erro de leitura:            00000100
+   Erro de escrita:            00001000
+*/
+
 int main(){
 
 	unsigned char status;
 
 	printf("Insira o numero do erro: \n");
 	scanf("%hhu", &status);
-	printf("%hhu", status);
+
+	if(status & (1 << 0)){
+	printf("Erro de Inicializacao");
+	}
 
 	return 0;
 }
