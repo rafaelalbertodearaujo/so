a11055a0f49bc41c3976960cb70896f1d237954c
rafaelalbertodearaujo <rafaelalbertola02@gmail.com>
2026-08-18 21:36:20 -0300

    erros

--- status.c
+++ status.c
@@ -15,8 +15,24 @@ int main(){
 	printf("Insira o numero do erro: \n");
 	scanf("%hhu", &status);
 
-	if(status & (1 << 0)){
-	printf("Erro de Inicializacao");
+	if(status == 0){
+		printf("Sem erros encontrados.\n");
+	}
+
+	if(status & (1 << 0)){ // 00000001 AND status = 00000001
+		printf("Erro de Inicializacao.\n");
+	}
+
+	if(status & (1 << 1)){ // 00000010 AND status = 00000010
+		printf("Erro de Escrita.\n");
+	}
+
+	if(status & (1 << 2)){ //00000100 AND status = 00000100
+		printf("Erro de Leitura\n");
+	}
+
+	if(status & (1 << 3)){ // 00001000 AND status = 00001000
+		printf("Erro\n");
 	}
 
 	return 0;
